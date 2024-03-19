8d1507ed88cd15fd6e66c91360e2e07519e64c77
Ордин Сергей Игоревич <osi@maynitek.ru>
2024-03-19 08:06:11 +0500

    update_19.03_08_05

--- distribution/urls.py
+++ distribution/urls.py
@@ -1,5 +1,10 @@
+from django.urls import path
+
 from distribution.apps import DistributionConfig
+from distribution.views import MailingEventListView
 
 app_name = DistributionConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path('', MailingEventListView.as_view(), name='mailingevent_list'),
+]
