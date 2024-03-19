ad93c63f76642ff8e7861d334c7b35a69dda9ee0
Ордин Сергей Игоревич <osi@maynitek.ru>
2024-03-19 08:18:12 +0500

    update_19.03_08_05

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,12 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+
+
+
