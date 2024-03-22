1658d9302c1db9dc3f9ecf9652a33124a42d48e7
Ордин Сергей Игоревич <osi@maynitek.ru>
2024-03-22 14:40:09 +0500

    fix users

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,8 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserLoginView, UserRegisterView, UserConfirmEmailView, EmailConfirmedView, EmailFailedView, \
-    EmailConfirmationSentView, UserProfileView, UserPasswordResetView, UserPasswordResetConfirmView
+from users.views import (UserLoginView, UserRegisterView, UserConfirmEmailView, UserProfileView, UserPasswordResetView,
+                         UserPasswordResetConfirmView)
 
 app_name = UsersConfig.name
 
@@ -13,9 +13,6 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('confirm_email/<str:token>/', UserConfirmEmailView.as_view(), name='confirm_email'),
-    path('email_confirmed/', EmailConfirmedView.as_view(), name='email_confirmed'),
-    path('email_failed/', EmailFailedView.as_view(), name='email_failed'),
-    path('email_confirmation_sent/', EmailConfirmationSentView.as_view(), name='email_confirmation_sent'),
     path('password_reset/', UserPasswordResetView.as_view(), name='password_reset'),
     path('set_new_password/<uidb64>/<token>/', UserPasswordResetConfirmView.as_view(), name='password_reset_confirm')
 
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta, timezone
 
+from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetConfirmView
@@ -9,7 +10,7 @@ from django.shortcuts import redirect
 from django.urls import reverse_lazy, reverse
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.views.generic import CreateView, View, TemplateView, UpdateView
+from django.views.generic import CreateView, View, UpdateView
 from jose import jwt, JWTError
 
 from config import settings
@@ -25,12 +26,15 @@ class UserLoginView(LoginView):
     template_name = 'users/login.html'
 
 
-class UserRegisterView(CreateView):
+class UserRegisterView(SuccessMessageMixin, CreateView):
     """ Представление для регистрации пользователя"""
     model = User
     form_class = UserRegisterForm
     template_name = 'users/register.html'
-    success_url = reverse_lazy('users:email_confirmation_sent')
+    success_url = reverse_lazy('home:home')
+    success_message = ("На ваш адрес электронной почты было отправлено письмо с подтверждением. "
+                       "Пожалуйста, проверьте свою электронную почту и нажмите на ссылку подтверждения, "
+                       "чтобы завершить регистрацию. Если письмо не пришло, проверьте папку спам.")
 
     def form_valid(self, form):
         user = form.save(commit=False)
@@ -61,6 +65,10 @@ class UserRegisterView(CreateView):
 class UserConfirmEmailView(View):
     """ Представление для обработки перехода по email пользователя"""
 
+    success_url = reverse_lazy('home:home')
+    error_message = ('Ссылка для подтверждения по электронной почте недействительна или срок ее действия истек. '
+                     'Пожалуйста, зарегистрируйтесь снова.')
+
     def get(self, request, token):
         try:
             payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
@@ -68,29 +76,20 @@ class UserConfirmEmailView(View):
             uid = urlsafe_base64_decode(uid_data)
             user = User.objects.get(pk=uid)
             if payload.get('exp') < datetime.now(timezone.utc).timestamp():
-                redirect('users:email_failed')
+                self.error_message = 'Срок действия ссылки истек. Пожалуйста, зарегистрируйтесь снова.'
+                messages.error(request, self.error_message)
+                return redirect(self.success_url)
+
         except (jwt.ExpiredSignatureError, JWTError, User.DoesNotExist):
-            return redirect('users:email_failed')
+            messages.error(request, self.error_message)
+            return redirect(self.success_url)
 
         user.is_active = True
         user.save()
         login(request, user)
-        return redirect('users:email_confirmed')
-
-
-class EmailConfirmedView(TemplateView):
-    """Подтверждение почты"""
-    template_name = 'users/email_confirmed.html'
-
-
-class EmailFailedView(TemplateView):
-    """Ошибка подтверждения почты"""
-    template_name = 'users/email_failed.html'
-
-
-class EmailConfirmationSentView(TemplateView):
-    """Отправка подтверждения почты"""
-    template_name = 'users/email_confirmation_sent.html'
+        success_message = 'Ваш адрес электронной почты успешно подтвержден. Спасибо за регистрацию!'
+        messages.success(request, success_message)
+        return redirect(self.success_url)
 
 
 class UserProfileView(LoginRequiredMixin, UpdateView):
